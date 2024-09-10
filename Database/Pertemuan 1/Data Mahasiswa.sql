1e089c6951c4b2ccaf9ff81d3ee0e546f2d80d11
diarboy <bigdickdumb@gmail.com>
2024-09-10 11:52:13 +0700

    create data mahasiswa

---
+++ Database/Pertemuan 1/Data Mahasiswa.sql
@@ -0,0 +1,12 @@
+INSERT INTO Mahasiswa (nama, nim, jurusan, angkatan)
+VALUES
+('Muh Fuad Azis', '623C0001', 'Ilmu Komputer', 2023),
+('Labiibul Muhsin', '623C0002', 'Sistem Informasi', 2024),
+('Dimas Dwi Ratna', '623C0003', 'Keperawatan', 2024),
+('Fasya Mahesa', '623C0004', 'Teknik Informatika', 2024),
+('Jaisy Bagel', '623C0005', 'Sistem Informasi', 2024),
+('Aurel Septian', '623C0006', 'Kebidanan', 2024),
+('Indah Rizkika', '623C0007', 'Kesehatan Masyarakat', 2024),
+('Muh Rizki', '623C0008', 'Teknik Informasi', 2024),
+('Ardi Syah', '623C0009', 'Teknik Informatika', 2024),
+('Sabil', '623C0010', 'Sistem Dakwah', 2024)
